9ea37a5e17c9e06fb9a8caceb28102589c333911
Pavel Novosadov <berk@mail.ru>
2017-04-09 15:06:13 +0300

    Create biki.h

---
+++ C/DZ/DZ030022017/DZ2/biki.h
@@ -0,0 +1,4 @@
+#pragma once  
+
+int bikiKorovi(int secret1, int secret2, int secret3, int secret4,
+	int chisloPolsovatelya1, int chisloPolsovatelya2, int chisloPolsovatelya3, int chisloPolsovatelya4);

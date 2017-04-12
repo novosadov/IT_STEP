2bba0db5aa4d94c0af71cc898c72eff1e1b18fa9
Pavel Novosadov <berk@mail.ru>
2017-04-12 11:52:34 +0300

    Update 120420172.c

--- C/InputOutput/120420172.c
+++ C/InputOutput/120420172.c
@@ -1,12 +1,42 @@
-#include<stdio.h>
-#include<locale.h>
-#include<stdlib.h>
-
-//ввод вывод данных
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+ 
+bool getFileName(char const* filePath,
+    char* fileName);
+ 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-
-	
-	return 0;
+    char* fileName = malloc(256);
+    if (getFileName("c:\\sdsdf",
+        fileName))
+    {
+        printf("filePath: %s\n", "c:\\sdsdf");
+        printf("fileName: %s", fileName);
+    }
+}
+ 
+bool getFileName(char const* filePath,
+    char* fileName)
+{
+    char* found = filePath;
+    char* tmp = NULL;
+    do
+    {
+        tmp =
+            strstr(found, "\\");
+ 
+        if (tmp != NULL)
+        {
+            found = tmp + 1;
+        }
+    } while (tmp);
+ 
+    if (found != NULL && found != filePath)
+    {
+        strcpy(fileName, found);
+        return true;
+    }
+    return false;
 }

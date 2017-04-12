614027d3706c90a497326a04db80b351affedfeb
Pavel Novosadov <berk@mail.ru>
2017-04-12 09:47:40 +0300

    Update 12042017.c

--- C/InputOutput/12042017.c
+++ C/InputOutput/12042017.c
@@ -1,9 +1,20 @@
 #include<stdio.h>
 #include<locale.h>
+#include<stdlib.h>
 
 //ввод вывод данных
-void main()
+int main()
 {
 	setlocale(LC_ALL, "rus");
 
+	FILE* file = fopen("c:\\windows\\system.ini", "r");   //открывает файл
+	char*  buff = malloc(1024);
+	while (fgets(buff, 1024, file))
+	{
+		printf(" %s\n", buff);
+	}
+
+	fclose(file); // закрывает файл
+
+	return 0;
 }

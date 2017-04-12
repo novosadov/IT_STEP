a78796ae1fe7e30ecb457f00e8623cc64ce51017
Pavel Novosadov <berk@mail.ru>
2017-04-12 10:06:34 +0300

    Update 12042017.c

--- C/InputOutput/12042017.c
+++ C/InputOutput/12042017.c
@@ -10,6 +10,9 @@ int main()
 	FILE* file = fopen("c:\\windows\\system.ini", "r");   //открывает файл
 
 	fseek(file, -25, SEEK_END);      //переместить вконец файла и отступить 25 от конца файла
+//	fseek(file, 25, SEEK_SET);       //переместить вначало файла и отступить 25 от начала
+//	fseek(file, 25, SEEK_CUR);       //ни куда не перемещает пропускает  по 25 байтов
+
 
 	char*  buff = malloc(256);
 	while (fgets(buff, 256, file))

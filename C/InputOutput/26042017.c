95dabc3e392410c36fe430ddb6ffcfaff981d121
Pavel Novosadov <berk@mail.ru>
2017-04-26 10:40:09 +0300

    Update 26042017.c

--- C/InputOutput/26042017.c
+++ C/InputOutput/26042017.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 //Бинарный режим
 
 struct Kurs
@@ -13,7 +12,6 @@ struct Kurs
 	float BynEuro;
 	float EuroByn;
 };
-
 
 int main()
 {
@@ -26,11 +24,10 @@ int main()
 
 		struct Kurs kurs1 = { 1.8600, 1.8700, 2.0350, 2.0470 };
 		fwrite(&kurs1, sizeof(struct Kurs), 1, f);
-
-		printf("%.4f\n", kurs1.BynUsd);
-		printf("%.4f\n", kurs1.UsdByn);
-		printf("%.4f\n", kurs1.BynEuro);
-		printf("%.4f\n", kurs1.EuroByn);
+		printf("Byn to usd  = \t%.4f\n", kurs1.BynUsd);
+		printf("Usd to byn = \t%.4f\n", kurs1.UsdByn);
+		printf("Byn to euro = \t%.4f\n", kurs1.BynEuro);
+		printf("Euro to byn = \t%.4f\n", kurs1.EuroByn);
 
 		fclose(f);
 		return 0;
@@ -38,11 +35,11 @@ int main()
 
 	struct Kurs kurs2;
 	fread(&kurs2, sizeof(struct Kurs), 1, f);
-
-	printf("%.4f\n", kurs2.BynUsd);
-	printf("%.4f\n", kurs2.UsdByn);
-	printf("%.4f\n", kurs2.BynEuro);
-	printf("%.4f\n", kurs2.EuroByn);	
+	printf("Byn to usd  = \t%.4f\n", kurs2.BynUsd);
+	printf("Usd to byn = \t%.4f\n", kurs2.UsdByn);
+	printf("Byn to euro = \t%.4f\n", kurs2.BynEuro);
+	printf("Euro to byn = \t%.4f\n", kurs2.EuroByn);	
 
 	fclose(f);
+	free(kurs);
 }

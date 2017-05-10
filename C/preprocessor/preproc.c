deef99246d7cd41e8a17307f2d98d5d4a02176c2
Pavel Novosadov <berk@mail.ru>
2017-05-10 11:24:51 +0300

    Create preproc.c

---
+++ C/preprocessor/preproc.c
@@ -0,0 +1,36 @@
+#include <stdbool.h>
+#include <Windows.h>
+#include<stdio.h>
+
+
+#define LOG_FN_START(arg) printf("[%d] %s\n", time(0),\
+#arg " start"); //Решётка превращает значение в строку, то есть #arg == "arg"
+
+#define LOG_FN_FINISH(arg) printf("[%d] %s\n", time(0),\
+#arg " end");
+
+
+void foo();
+void foo1();
+
+void foo()
+{
+	LOG_FN_START(foo)
+	Sleep(1000);
+	foo1();
+	LOG_FN_FINISH(foo)
+}
+
+void foo1()
+{
+	LOG_FN_START(foo1)
+	Sleep(1000);
+	LOG_FN_FINISH(foo1)
+}
+
+int main()
+{
+	LOG_FN_START(main)
+	foo();
+	LOG_FN_FINISH(main)
+}

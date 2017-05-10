b333f2e9713796b107af1e550a10b6fa98a5d663
Pavel Novosadov <berk@mail.ru>
2017-05-10 15:32:08 +0300

    Update exam2.c

--- C/Exam/exam2.c
+++ C/Exam/exam2.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#include <direct.h> //Для создания папки
+#include<direct.h> //Для создания папки
+#include<Windows.h>
 
 /*
 Разработать программу хранения рецептов.
@@ -33,15 +34,13 @@
 Очистка файла: можно открыть файл в режиме "w" и закрыть.
 */
 
-
-struct Recipe        
+struct Recipe
 {
 	char name[256];
 	char date[50];
 	char text[1024];
 	char reiting[10];
 };
-
 
 enum Menu
 {
@@ -58,24 +57,28 @@ void clear();
 
 
 int main()
-{	
-	struct Recipe* recipe = malloc(1024 * sizeof(struct Recipe*));		
+{
+	struct Recipe* recipe = malloc(1024 * sizeof(struct Recipe*));
 	printf(" ================ Book of recipes ================ \n\n");
 	menu(recipe);
-	free(recipe);	
-	return 0; 
+	free(recipe);
+	return 0;
 }
 
 
 void menu(struct Recipe recipe[])
-{	
+{
 	char* fname = (char *)malloc(256);
 	char* disk = (char *)malloc(50);
 	char* folder = (char *)malloc(256);
 	char* path = (char *)malloc(256);
 	char* dir = (char *)malloc(256);
-	char* text = (char*)malloc(1024);	
-	
+	char* dir2 = (char *)malloc(256);
+	char* text = (char*)malloc(10240);
+
+	WIN32_FIND_DATAA found;
+	HANDLE findeResult;
+
 	while (true)
 	{
 		int count = 0;
@@ -83,7 +86,7 @@ void menu(struct Recipe recipe[])
 		printf("Select a menu item\n");
 		printf("0 - Exit\n1 - Add recipe\n2 - Vie recipe\n3 - Edit recipe\n4 - Delete reciep\n");
 		scanf("%i", &menu);
-		clear();		
+		clear();
 		FILE* fp;
 		switch (menu)
 		{
@@ -91,29 +94,30 @@ void menu(struct Recipe recipe[])
 			return 0;
 			break;
 
+				
 		case AddRecipe:
-			printf("Enter name disk\n");			
+			printf("Enter name disk\n");
 			fgets(disk, 50, stdin);
 
-			printf("Enter name folder\n");			
+			printf("Enter name folder\n");
 			fgets(folder, 256, stdin);
 
 			printf("Enter name recipe\n");
 			fgets(fname, 256, stdin);
 
-			char* nameRecipe[256];			
+			char* nameRecipe[256];
 			strcpy(nameRecipe, fname);
-				
+
 			disk[strlen(disk) - 1] = '\0';
 			folder[strlen(folder) - 1] = '\0';
 			fname[strlen(fname) - 1] = '\0';
 
 			strcpy(path, disk);
 			strcat(path, ":\\");
-			strcat(path, folder);	
-			strcat(fname, ".recipe");				
-			mkdir(path);					
-			strcpy(dir, path);	
+			strcat(path, folder);
+			strcat(fname, ".recipe");
+			mkdir(path);
+			strcpy(dir, path);
 			strcat(dir, "\\");
 			strcat(dir, fname);
 
@@ -124,45 +128,61 @@ void menu(struct Recipe recipe[])
 				getch();
 				return 0;
 			}
-			
-			struct Recipe f;			
+
+			struct Recipe f;
 			strcpy(f.name, &nameRecipe);
 			printf("Enter date recipe\n");
 			scanf("%s", &f.date);
 			printf("Enter text recipe\n");
 			scanf("%s", &f.text);
 			printf("Enter reiting\n");
-			scanf("%s", &f.reiting);		
+			scanf("%s", &f.reiting);
 			clear();
 
 			fwrite(&f, sizeof(struct Recipe), 1, fp);
 			fclose(fp);
-			count ++;
+			count++;
 			break;
-			
-		case VieRecipe:				
 
+				
+		case VieRecipe:
 			printf("Enter name disk\n");
 			fgets(disk, 50, stdin);
 
 			printf("Enter name folder\n");
-			fgets(folder, 256, stdin);
-			
-
-			printf("Enter name recipe\n");
-			fgets(fname, 256, stdin);
+			fgets(folder, 256, stdin);			
 
 			disk[strlen(disk) - 1] = '\0';
-			folder[strlen(folder) - 1] = '\0';
-			fname[strlen(fname) - 1] = '\0';
+			folder[strlen(folder) - 1] = '\0';				
 
 			strcpy(path, disk);
 			strcat(path, ":\\");
-			strcat(path, folder);	
-			strcat(fname, ".recipe");
+			strcat(path, folder);			
 			mkdir(path);
 			strcpy(dir, path);
 			strcat(dir, "\\");
+			strcpy(dir2, dir);
+			strcat(dir2, "*.recipe");
+
+			findeResult = FindFirstFileA(dir2,
+				&found);
+
+			if (findeResult == INVALID_HANDLE_VALUE)
+			{
+				printf("Files not found\n");
+				return 1;
+			}
+			do
+			{
+				printf("Recipe: %s\n", found.cFileName);
+			} while (FindNextFileA(findeResult, &found));
+			
+			FindClose(findeResult);
+
+			printf("Enter name recipe\n");
+			fgets(fname, 256, stdin);
+			fname[strlen(fname) - 1] = '\0';
+			strcat(fname, ".recipe");
 			strcat(dir, fname);
 
 			fp = fopen(dir, "r");
@@ -172,21 +192,22 @@ void menu(struct Recipe recipe[])
 				getch();
 				return 0;
 			}
-			
+
 			struct Recipe f2;
-			fread(&f2, sizeof(struct Recipe),1, fp);
+			fread(&f2, sizeof(struct Recipe), 1, fp);
 			printf("Name recipe: \n\t%s", f2.name);
 			printf("========================================================================\n");
 			printf("Date recipe: \n\t%s\n", f2.date);
 			printf("========================================================================\n");
 			printf("Text recipe: \n\t%s\n", f2.text);
 			printf("========================================================================\n");
-			printf("Reiting: \n\t%s\n", f2.reiting);	
+			printf("Reiting: \n\t%s\n", f2.reiting);
 			printf("========================================================================\n");
 
 			fclose(fp);
 			break;
 
+				
 		case EditRecipe:
 			printf("Enter name disk\n");
 			fgets(disk, 50, stdin);
@@ -194,20 +215,37 @@ void menu(struct Recipe recipe[])
 			printf("Enter name folder\n");
 			fgets(folder, 256, stdin);
 
-			printf("Enter name recipe\n");
-			fgets(fname, 256, stdin);
-
 			disk[strlen(disk) - 1] = '\0';
 			folder[strlen(folder) - 1] = '\0';
-			fname[strlen(fname) - 1] = '\0';
 
 			strcpy(path, disk);
 			strcat(path, ":\\");
-			strcat(path, folder);			
-			strcat(fname, ".recipe");
+			strcat(path, folder);
 			mkdir(path);
 			strcpy(dir, path);
 			strcat(dir, "\\");
+			strcpy(dir2, dir);
+			strcat(dir2, "*.recipe");
+
+			findeResult = FindFirstFileA(dir2,
+				&found);
+
+			if (findeResult == INVALID_HANDLE_VALUE)
+			{
+				printf("Files not found\n");
+				return 1;
+			}
+			do
+			{
+				printf("Recipe: %s\n", found.cFileName);
+			} while (FindNextFileA(findeResult, &found));
+
+			FindClose(findeResult);
+
+			printf("Enter name recipe\n");
+			fgets(fname, 256, stdin);
+			fname[strlen(fname) - 1] = '\0';
+			strcat(fname, ".recipe");
 			strcat(dir, fname);
 
 			fp = fopen(dir, "a");
@@ -217,21 +255,18 @@ void menu(struct Recipe recipe[])
 				getch();
 				return 0;
 			}
-			
+
 			struct Recipe f3;
-			strcpy(f3.name, &nameRecipe);
-			printf("Enter date recipe\n");
-			scanf("%s", &f3.date);
+			strcpy(f3.name, &nameRecipe);			
 			printf("Enter text recipe\n");
-			scanf("%s", &f3.text);
-			printf("Enter reiting\n");
-			scanf("%s", &f3.reiting);
+			scanf("%s", &f3.text);			
 			clear();
 
 			fwrite(&f3, sizeof(struct Recipe), 1, fp);
 			fclose(fp);
 			break;
 
+				
 		case DelReciep:
 			printf("Enter name disk\n");
 			fgets(disk, 50, stdin);
@@ -248,13 +283,13 @@ void menu(struct Recipe recipe[])
 
 			strcpy(path, disk);
 			strcat(path, ":\\");
-			strcat(path, folder);			
+			strcat(path, folder);
 			strcat(fname, ".recipe");
 			mkdir(path);
 			strcpy(dir, path);
 			strcat(dir, "\\");
 			strcat(dir, fname);
-			
+
 			if (fp == NULL)
 			{
 				printf("Error.Ne udalos otkrit fail!\n");
@@ -268,20 +303,20 @@ void menu(struct Recipe recipe[])
 			else
 			{
 				printf("Ura ydalili!!!\n");
-			}			
+			}
 			break;
 
 		default:
 			printf("You have entered not right value...\n");
 			break;
 		}
-		
+
 	}
 	free(fname);
 	free(disk);
 	free(folder);
 	free(path);
-	free(dir);	
+	free(dir);
 }
 
 

2a3457fba5eaee38e0581f38ef415fac879d61bf
Pavel Novosadov <berk@mail.ru>
2017-05-10 10:42:27 +0300

    Update search2.c

--- C/Exam/search2.c
+++ C/Exam/search2.c
@@ -1,39 +1,41 @@
-#include<stdio.h>
-#include<locale.h>
-#include<Windows.h>
-#include<stdbool.h>
-
-
+#include <Windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+ 
 int main()
 {
-	WIN32_FIND_DATAA found;
-	setlocale(LC_ALL, "rus");
-	HANDLE findeResult = FindFirstFileA("D:\\*.*", 
-		&found);
-
-	if (findeResult == INVALID_HANDLE_VALUE)
-	{
-		printf("Files not found\n");
-		return 1;
-	}
-
-	do
-	{
-		if (strcmp(found.cFileName,".")==0||
-			strcmp(found.cFileName, "..")==0)
-		{
-			continue;
-		} 
-		
-		bool isDirectory = 
-			found.dwFileAttributes &
-			FILE_ATTRIBUTE_DIRECTORY;
-		printf("%d ", isDirectory);
-		printf("File found %s\n", found.cFileName);
-	} while (FindNextFileA(findeResult, &found));
-	
-
-	FindClose(findeResult);
-
-	return 0;
+    WIN32_FIND_DATAA found;
+    HANDLE findResult =
+        FindFirstFileA(
+            "d:\\winners\\*.jpg",
+            &found);
+ 
+    if (findResult == INVALID_HANDLE_VALUE)
+    {
+        printf("Files not found.");
+        return 1;
+    }
+    // WinAPI
+    do
+    {
+        if (strcmp(found.cFileName, ".") == 0 ||
+            strcmp(found.cFileName, "..") == 0)
+        {
+            continue;
+        }
+       
+        SYSTEMTIME time;
+        FileTimeToSystemTime(
+            &found.ftLastAccessTime, &time);
+        printf("[%d.%d.%d] ", time.wDay, time.wMonth, time.wYear);
+        bool isDirectory =
+            found.dwFileAttributes &
+            FILE_ATTRIBUTE_DIRECTORY;
+        printf("%s found: \"%s\".\n",
+            (isDirectory ? "Folder" : "File"),
+            found.cFileName);
+    } while (FindNextFileA(findResult,
+        &found));
+ 
+    FindClose(findResult);
 }

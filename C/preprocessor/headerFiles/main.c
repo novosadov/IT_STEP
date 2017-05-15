dd54e573e479df22d828c4b8aa9e79222e490576
Pavel Novosadov <berk@mail.ru>
2017-05-15 11:16:55 +0300

    Create main.c

---
+++ C/preprocessor/headerFiles/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<locale.h>
+#include"Car.h"
+#include"Boat.h"
+#include"Transport.h"
+
+
+void main()
+{
+	enum CarType carType1 = CarTypeCargo;
+	enum BoatType boatType1 = BoatTypeCargo;
+	carStart();
+	carStop();
+
+}

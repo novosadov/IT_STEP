d8fd237261b5d4fef6bcba73afa5e47aeadf354d
Pavel Novosadov <berk@mail.ru>
2017-05-15 10:42:08 +0300

    Update version.c

--- C/preprocessor/version.c
+++ C/preprocessor/version.c
@@ -1,1 +1,32 @@
-https://pastebin.com/UdVSYZx6
+#include <stdio.h>
+ //выводит версию при помощи макросов
+void max1()
+{
+    printf("%s", __FUNCTION__);
+}
+ 
+void max2()
+{
+    printf(__FUNCTION__);
+}
+ 
+void max3()
+{
+    printf(__FUNCTION__);
+}
+ 
+void min1() { printf(__FUNCTION__); }
+void min2() { printf(__FUNCTION__); }
+void min3() { printf(__FUNCTION__); }
+ 
+#define VERSION 2
+ 
+#define EXEC2(foo, version) foo ## version();
+#define EXEC1(foo, version) EXEC2(foo, version)
+#define EXEC(foo) EXEC1(foo, VERSION)
+ 
+int main(int argCount, char* argValues[])
+{
+    EXEC(max);
+    EXEC(min);
+}

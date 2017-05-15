3c574d111899dc36b09998d417ab8642399e8ed3
Pavel Novosadov <berk@mail.ru>
2017-05-15 11:17:22 +0300

    Create Car.h

---
+++ C/preprocessor/headerFiles/Car.h
@@ -0,0 +1,15 @@
+#ifndef CAR_H
+
+enum CarType
+{
+	CarTypeCargo,
+	CarTypePassenger
+};
+
+void carStart();
+void carStop();
+void carSpeedUp();
+void carSlowDown();
+
+#endif //CAR_H
+#define CAR_H

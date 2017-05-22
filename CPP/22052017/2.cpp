dbc3ec6e4a11ed12dd4e92b502e77acc1a40befe
Pavel Novosadov <berk@mail.ru>
2017-05-22 10:02:53 +0300

    Create 2.cpp

---
+++ CPP/22052017/2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include "ArrayUtils.h"
+#include<locale.h>
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+
+void main()
+{
+	int a = 100;
+	double b = 20.5;
+
+	int& refa = a;
+	double& refb = b;
+	std::cout << a << " " << refa << "\n";
+
+	Point p = { 3,5 };
+	Point& refp = p;
+	refp.x = 6;
+}

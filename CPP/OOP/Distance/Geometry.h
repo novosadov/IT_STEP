f3b82eaf814d6291b7b5a5515491edae350a18b5
Pavel Novosadov <berk@mail.ru>
2017-05-23 11:50:48 +0300

    Update Geometry.h

--- CPP/OOP/Distance/Geometry.h
+++ CPP/OOP/Distance/Geometry.h
@@ -3,54 +3,65 @@
 class Point
 {
 private:
-    int m_x;
-    int m_y;
-    
+	int m_x;
+	int m_y;
+
 public:
-    int GetX();
-    int GetY();
-    void Init(int x, int y);
-    void Read();
-    void Print();
-    void Move(int x, int y);
+	Point();
+	Point(int x, int y);
+
+	int GetX();
+	int GetY();
+	void Init(int x, int y);
+	void Read();
+	void Print();
+	void Move(int x, int y);
 };
 
 class Size
 {
 private:
-    int m_width;
-    int m_heigh;
-    
+	int m_width;
+	int m_heigh;
+
 public:
-    int GetWidth();
-    int GetHeight();
-    void Init(int width, int height);
-    void Read();
-    void Print();
-    void Scale(float xscale, float yscale);
-    bool IsEmpty();
+	Size();
+	Size(int width, int height);
+
+	int GetWidth();
+	int GetHeight();
+	void Init(int width, int height);
+	void Read();
+	void Print();
+	void Scale(float xscale, float yscale);
+	bool IsEmpty();
 };
 
 class Rectangle
 {
 private:
-    Point m_origin;
-    Size m_size;
-    
-    int GetLeft();
-    int GetRight();
-    int GetTop();
-    int GetBottom();
-    
+	Point m_origin;
+	Size m_size;
+
+	int GetLeft();
+	int GetRight();
+	int GetTop();
+	int GetBottom();
+
 public:
-    Point GetOrigin();
-    Size GetSize();
-    void Init(int x, int y, int width, int height);
-    void Init(Point const& origin, Size const& size);
-    void Move(int x, int y);
-    bool IsEmpty();
-    bool Contains(Point& p);
-    Rectangle IntersectWith(Rectangle& r);
-    void Read();
-    void Print();
+	Rectangle();
+	Rectangle(int x, int y, int width, int height);
+	Rectangle(Point const& origin, Size const& size);
+
+
+	Point GetOrigin();
+	Size GetSize();
+	void Init(int x, int y, int width, int height);
+	void Init(Point const& origin, Size const& size);
+	void Move(int x, int y);
+	bool IsEmpty();
+	bool Contains(Point& p);
+	Rectangle IntersectWith(Rectangle& r);
+	void Read();
+	void Print();
 };

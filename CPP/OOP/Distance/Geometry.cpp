2f2e5b00a711e1dbf69528f5b42b78bc60be964a
Pavel Novosadov <berk@mail.ru>
2017-05-23 11:49:58 +0300

    Update Geometry.cpp

--- CPP/OOP/Distance/Geometry.cpp
+++ CPP/OOP/Distance/Geometry.cpp
@@ -4,176 +4,220 @@
 
 // Point
 
+Point::Point()
+{
+	m_x = 0;
+	m_y = 0;
+}
+
+
+Point::Point(int x, int y)
+{
+	m_x = x;
+	m_y = y;
+}
+
 int Point::GetX()
 {
-    return m_x;
+	return m_x;
 }
 
 int Point::GetY()
 {
-    return m_y;
+	return m_y;
 }
 
 void Point::Init(int x, int y)
 {
-    m_x = x;
-    m_y = y;
+	m_x = x;
+	m_y = y;
 }
 
 void Point::Read()
 {
-    std::cout << "Enter point coordinates: x y\n";
-    std::cin >> m_x >> m_y;
+	std::cout << "Enter point coordinates: x y\n";
+	std::cin >> m_x >> m_y;
 }
 
 void Point::Print()
 {
-    std::cout << m_x << ", " << m_y << "\n";
+	std::cout << m_x << ", " << m_y << "\n";
 }
 
 void Point::Move(int x, int y)
 {
-    m_x += x;
-    m_y += y;
+	m_x += x;
+	m_y += y;
 }
 
 // Size
+
+Size::Size()
+{
+	m_width = 0;
+	m_heigh = 0;
+}
+Size::Size(int width, int height)
+{
+	m_width = width;
+	m_heigh = height;
+}
+
+
+
 int Size::GetWidth()
 {
-    return m_width;
+	return m_width;
 }
 
 int Size::GetHeight()
 {
-    return m_heigh;
+	return m_heigh;
 }
 
 void Size::Init(int width, int height)
 {
-    m_width = width;
-    m_heigh = height;
+	m_width = width;
+	m_heigh = height;
 }
 
 void Size::Read()
 {
-    std::cout << "Enter width height\n";
-    std::cin >> m_width >> m_heigh;
+	std::cout << "Enter width height\n";
+	std::cin >> m_width >> m_heigh;
 }
 
 void Size::Print()
 {
-    std::cout << m_width << ", " << m_heigh << "\n";
+	std::cout << m_width << ", " << m_heigh << "\n";
 }
 
 void Size::Scale(float xscale, float yscale)
 {
-    m_width *= xscale;
-    m_heigh *= yscale;
+	m_width *= xscale;
+	m_heigh *= yscale;
 }
 
 bool Size::IsEmpty()
 {
-    return m_width == 0 || m_heigh == 0;
+	return m_width == 0 || m_heigh == 0;
 }
 
 // Rectangle
 
+
+Rectangle::Rectangle()
+{
+	m_origin.Init(0, 0);
+	m_size.Init(0, 0);
+}
+Rectangle::Rectangle(int x, int y, int width, int height):
+	m_origin(x, y),
+	m_size(width, height)
+{
+}
+Rectangle::Rectangle(Point const& origin, Size const& size) :
+	m_origin(origin),
+	m_size(size)
+{
+}
+
 Point Rectangle::GetOrigin()
 {
-    return m_origin;
+	return m_origin;
 }
 
 Size Rectangle::GetSize()
 {
-    return m_size;
+	return m_size;
 }
 
 int Rectangle::GetLeft()
 {
-    return m_origin.GetX();
+	return m_origin.GetX();
 }
 
 int Rectangle::GetRight()
 {
-    return m_origin.GetX() + m_size.GetWidth();
+	return m_origin.GetX() + m_size.GetWidth();
 }
 
 int Rectangle::GetTop()
 {
-    return m_origin.GetY() + m_size.GetHeight();
+	return m_origin.GetY() + m_size.GetHeight();
 }
 
 int Rectangle::GetBottom()
 {
-    return m_origin.GetY();
+	return m_origin.GetY();
 }
 
 void Rectangle::Init(int x, int y, int width, int height)
 {
-    m_origin.Init(x, y);
-    m_size.Init(width, height);
+	m_origin.Init(x, y);
+	m_size.Init(width, height);
 }
 
 void Rectangle::Init(Point const& origin, Size const& size)
 {
-    m_origin = origin;
-    m_size = size;
+	m_origin = origin;
+	m_size = size;
 }
 
 void Rectangle::Move(int x, int y)
 {
-    m_origin.Move(x, y);
+	m_origin.Move(x, y);
 }
 
 bool Rectangle::IsEmpty()
 {
-    return m_size.IsEmpty();
+	return m_size.IsEmpty();
 }
 
 bool Rectangle::Contains(Point& p)
 {
-    return GetLeft() <= p.GetX() && p.GetX() <= GetRight() && GetBottom() <= p.GetY() && p.GetY() <= GetTop();
+	return GetLeft() <= p.GetX() && p.GetX() <= GetRight() && GetBottom() <= p.GetY() && p.GetY() <= GetTop();
 }
 
 Rectangle Rectangle::IntersectWith(Rectangle& r)
 {
-    Rectangle result;
-    result.Init(0, 0, 0, 0);
-    int left = GetLeft();
-    if (r.GetLeft() > left)
-    {
-        left = r.GetLeft();
-    }
-    int right = GetRight();
-    if (r.GetRight() < right)
-    {
-        right = r.GetRight();
-    }
-    int bottom = GetBottom();
-    if (r.GetBottom() > bottom)
-    {
-        bottom = r.GetBottom();
-    }
-    int top = GetTop();
-    if (r.GetTop() < top)
-    {
-        top = r.GetTop();
-    }
-    if (right > left && top > bottom)
-    {
-        result.Init(left, bottom, right - left, top - bottom);
-    }
-    return result;
+	Rectangle result;
+	result.Init(0, 0, 0, 0);
+	int left = GetLeft();
+	if (r.GetLeft() > left)
+	{
+		left = r.GetLeft();
+	}
+	int right = GetRight();
+	if (r.GetRight() < right)
+	{
+		right = r.GetRight();
+	}
+	int bottom = GetBottom();
+	if (r.GetBottom() > bottom)
+	{
+		bottom = r.GetBottom();
+	}
+	int top = GetTop();
+	if (r.GetTop() < top)
+	{
+		top = r.GetTop();
+	}
+	if (right > left && top > bottom)
+	{
+		result.Init(left, bottom, right - left, top - bottom);
+	}
+	return result;
 }
 
 void Rectangle::Read()
 {
-    m_origin.Read();
-    m_size.Read();
+	m_origin.Read();
+	m_size.Read();
 }
 
 void Rectangle::Print()
 {
-    std::cout << "x: " << m_origin.GetX() << "y: " << m_origin.GetY() <<
-        "width: " << m_size.GetWidth() << "height: " << m_size.GetHeight() << std::endl;
+	std::cout << "x: " << m_origin.GetX() << "y: " << m_origin.GetY() <<
+		"width: " << m_size.GetWidth() << "height: " << m_size.GetHeight() << std::endl;
 }

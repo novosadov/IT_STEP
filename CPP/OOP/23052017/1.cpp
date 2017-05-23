43367d8c23f9e91c3559d3f5d2d451aa460926df
Pavel Novosadov <berk@mail.ru>
2017-05-23 09:56:40 +0300

    Create 1.cpp

---
+++ CPP/OOP/23052017/1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+class Distance
+{
+private:
+	int m_feet;
+	float m_inches;
+
+public:
+	int GetFeet()
+	{
+		return m_feet;
+	};
+	float GetInches()
+	{
+		return m_inches;
+	};
+	void SetData(int feet, float inches)
+	{
+		m_feet = feet;
+		m_inches = inches;
+	};
+	void Read()
+	{
+		std::cin >> m_feet >> m_inches;
+	};
+	void Print()
+	{
+		std::cout << m_feet << "\'" << m_inches << "\"" << std::endl;
+	};
+};
+
+int main()
+{
+	
+	
+	
+
+	return 0;
+}

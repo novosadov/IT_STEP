ccdfca47c7ba646f1356999634a373d3da9fb92e
Pavel Novosadov <berk@mail.ru>
2017-05-24 11:10:14 +0300

    Create Students.cpp

---
+++ CPP/OOP/Students/Students.cpp
@@ -0,0 +1,71 @@
+#include "Student.h"
+
+#include <cstring>
+#include <iostream>
+
+Student::Student(Group& group) :
+	m_group(group),
+	m_rating(0)
+{
+	m_name = new char[1];
+	m_name[0] = '\0';
+}
+
+Student::Student(Group& group, char const* name, int rating) :
+	m_name(NULL),
+	m_group(group),
+	m_rating(rating)
+{
+	SetName(name);
+}
+
+Student::Student(Student const& student) :
+	m_group(student.m_group),
+	m_rating(student.m_rating)
+{
+	std::cout << "copy ctor\n";
+	SetName(student.GetName());
+}
+
+Student::~Student()
+{
+	delete[] m_name;
+}
+
+char const* Student::GetName() const
+{
+	return m_name;
+}
+
+int Student::GetRating()
+{
+	return m_rating;
+}
+
+void Student::SetName(char const* name)
+{
+	delete[] m_name;
+	size_t length = strlen(name);
+	m_name = new char[length + 1];
+	strcpy(m_name, name);
+}
+
+void Student::SetRating(int rating)
+{
+	m_rating = rating;
+}
+
+void Student::Read()
+{
+	std::cout << "Enter name: ";
+	char buffer[21];
+	std::cin.getline(buffer, 20);
+	SetName(buffer);
+	std::cout << "Enter rating: ";
+	std::cin >> m_rating;
+}
+
+void Student::Print()
+{
+	std::cout << m_name << ": " << m_rating << "\n";
+}

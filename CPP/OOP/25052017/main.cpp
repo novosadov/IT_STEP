91796c6ff6c6dd10486b3cd9f1e99e6bfa955f76
Pavel Novosadov <berk@mail.ru>
2017-05-25 09:36:07 +0300

    Create main.cpp

---
+++ CPP/OOP/25052017/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include "Student.h"
+// Конструкторы
+
+class A
+{
+	const int a;
+
+public:
+	A(int a) :
+		a(a)
+	{
+	}
+};
+
+A ga(1);
+
+class B
+{
+	A& a;
+public:
+	B() :
+		a(ga)
+	{
+	}
+
+	B(A& a) :
+		a(a)
+	{
+	}
+};
+
+void PrintMaxRaiting(Student s1, Student s2)
+{
+	std::cout << (s1.GetRating() < s2.GetRating() ?
+		s2.GetRating() : s1.GetRating()) << "\n";
+}
+
+Student Create(Group& g, char const* name)
+{
+	Student s(g, name, 0);
+	return s;
+}
+
+int main()
+{
+	/*
+	A a1(100);
+	B b(a1);
+
+	Group g;
+	Student s1(g, "Ivanov", 6);
+	Student s2(g);
+	s2 = s1;
+	s2.SetName("Petrov");
+	
+	s1.Print();
+	s2.Print();	
+	*/
+
+	Group g;
+	Student s1(g, "Ivanov", 6);
+	Student s2 = s1;
+	Student s3(s1);
+
+	std::cout << "count = " << Student::GetStudentCount() << "\n";
+
+	s2.SetName("Petrov");
+	s2.SetRating(5);
+	s1.Print();
+	s2.Print();
+
+	PrintMaxRaiting(s1, s2);
+	Student* s5;
+
+	{
+		std::cout << "count = " << Student::GetStudentCount() << "\n";
+		s5 = new Student(g);
+		Student s4 = Create(g, "Sidorov");
+		s4.Print();
+		std::cout << "count = " << Student::GetStudentCount() << "\n";
+	}
+	std::cout << "count = " << Student::GetStudentCount() << "\n";
+	delete s5;
+	std::cout << "count = " << Student::GetStudentCount() << "\n";
+
+	return 0;
+}

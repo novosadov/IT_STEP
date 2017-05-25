dc95c31bf8a665308cb743debbb8ccb78be82457
Pavel Novosadov <berk@mail.ru>
2017-05-25 11:47:01 +0300

    Update Distance.h

--- CPP/OOP/25052017/Distance.h
+++ CPP/OOP/25052017/Distance.h
@@ -7,18 +7,15 @@ public:
 	Distance(int feet, float inches);
 	Distance(int feet);
 
-	int GetFeet();
-	float GetInches();
+	int GetFeet() const;
+	float GetInches() const;
 	void SetData(int feet, float inches);
 	void Read();
 	void Print();
-	void Add(Distance const& d);
-	void Deduct(Distance const& d);
 
-
-	Distance operator-(Distance const& rhs);
 	Distance operator+(Distance const& rhs);
 	void operator+=(Distance const& rhs);
+	Distance operator-(Distance const& rhs);
 	void operator-=(Distance const& rhs);
 	Distance& operator++();
 	Distance operator++(int);
@@ -28,6 +25,4 @@ private:
 	float m_inches = 0;
 };
 
-
-//Distance Summ(Distance const& d1, Distance const& d2);
-//Distance Diff(Distance const& d1, Distance const& d2);
+float operator/(Distance const& lhs, Distance const& rhs);

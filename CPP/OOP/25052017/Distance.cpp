91d3e016291e5a275a49313c26b195b8a77a4300
Pavel Novosadov <berk@mail.ru>
2017-05-25 11:46:22 +0300

    Update Distance.cpp

--- CPP/OOP/25052017/Distance.cpp
+++ CPP/OOP/25052017/Distance.cpp
@@ -1,183 +1,104 @@
 #include "Distance.h"
-
+ 
 #include <iostream>
-
-// without using initialization list
-/*
+ 
 Distance::Distance()
 {
-m_feet = 0;
-m_inches = 0;
+    m_feet = 0;
+    m_inches = 0;
 }
-Distance::Distance(int feet)
-{
-m_feet = feet;
-m_inches = 0;
-}
+ 
 Distance::Distance(int feet, float inches)
 {
-m_feet = feet;
-m_inches = inches;
+    m_feet = feet;
+    m_inches = inches;
+    // SetData(feet, inches);
 }
-*/
-
-// using initialization list
-
-Distance::Distance() :
-	m_feet(0),
-	m_inches(0)
+ 
+Distance::Distance(int feet)
 {
+    m_feet = feet;
+    m_inches = 0;
 }
-
-Distance::Distance(int feet, float inches) :
-	m_feet(feet),
-	m_inches(inches)
+ 
+int Distance::GetFeet() const
 {
+    return m_feet;
 }
-
-// delegating constructors: calling constructor from constructor
-
-Distance::Distance(int feet) :
-	Distance(feet, 0)
+ 
+float Distance::GetInches() const
 {
+    return m_inches;
 }
-
-int Distance::GetFeet() 
-{
-	return m_feet;
-}
-
-float Distance::GetInches() 
-{
-	return m_inches;
-}
-
+ 
 void Distance::SetData(int feet, float inches)
 {
-	m_feet = feet;
-	m_inches = inches;
+    m_feet = feet;
+    m_inches = inches;
 }
-
+ 
 void Distance::Read()
 {
-	std::cin >> m_feet >> m_inches;
+    std::cin >> m_feet >> m_inches;
 }
-
-void Distance::Print() 
+ 
+void Distance::Print()
 {
-	std::cout << m_feet << "\'" << m_inches << "\"" << std::endl;
+    std::cout << m_feet << "\'" << m_inches << "\"" << std::endl;
 }
-
-void Distance::Add(Distance const& d)
-{
-m_feet += d.m_feet;
-m_inches += d.m_inches;
-if (m_inches >= 12.0f)
-{
-m_inches -= 12.0f;
-m_feet += 1;
-}
-}
-void Distance::Deduct(Distance const& d)
-{
-m_feet -= d.m_feet;
-m_inches -= d.m_inches;
-if (m_inches < 0)
-{
-m_inches += 12.0f;
-m_feet -= 1;
-}
-}
-/*Distance Summ(Distance const& d1, Distance const& d2)
-{
-Distance result = d1;
-result.Add(d2);
-return result;
-}
-Distance Diff(Distance const& d1, Distance const& d2)
-{
-Distance result = d1;
-result.Deduct(d2);
-return result;
-}
-*/
-
-//float Distance::ToInches() const
-//{
-//	return m_feet * 12.0f + m_inches;
-//}
-
-
-
-
-//
-//bool Distance::operator==(Distance const& rhs) const
-//{
-//	return ToInches() == rhs.ToInches();
-//}
-//
-//bool Distance::operator!=(Distance const& rhs) const
-//{
-//	return !(*this == rhs);
-//}
-//
-//bool Distance::operator<=(Distance const& rhs) const
-//{
-//	return ToInches() <= rhs.ToInches();
-//}
-//
-//bool Distance::operator>(Distance const& rhs) const
-//{
-//	return !(*this <= rhs);
-//}
-
-
-
-void Distance::operator+=(Distance const& rhs)
-{
-	m_feet += rhs.m_feet;
-	m_inches += rhs.m_inches;
-	if (m_inches >= 12.0f)
-	{
-		m_inches -= 12.0f;
-		m_feet += 1;
-	}
-}
-
+ 
 Distance Distance::operator+(Distance const& rhs)
 {
-	Distance result = *this;
-	result += rhs;
-	return result;
+    Distance result(*this);
+    result += rhs;
+    return result;
 }
-
-void Distance::operator-=(Distance const& rhs)
+ 
+void Distance::operator+=(Distance const& rhs)
 {
-	m_feet -= rhs.m_feet;
-	m_inches -= rhs.m_inches;
-	if (m_inches < 0)
-	{
-		m_inches += 12.0f;
-		m_feet -= 1;
-	}
+    m_feet += rhs.m_feet;
+    m_inches += rhs.m_inches;
+    if (m_inches >= 12.0f)
+    {
+        m_inches -= 12.0f;
+        m_feet += 1;
+    }
 }
-
+ 
 Distance Distance::operator-(Distance const& rhs)
 {
-	Distance result = *this;
-	result -= rhs;
-	return result;
+    Distance result(*this);
+    result -= rhs;
+    return result;
 }
-
+ 
+void Distance::operator-=(Distance const& rhs)
+{
+    m_feet -= rhs.m_feet;
+    m_inches -= rhs.m_inches;
+    if (m_inches < 0)
+    {
+        m_inches += 12.0f;
+        m_feet -= 1;
+    }
+}
+ 
 Distance& Distance::operator++()
 {
-	m_feet += 1;
-	return *this;
+    m_feet++;
+    return *this;
 }
-
+ 
 Distance Distance::operator++(int)
 {
-	Distance copy(*this); //копируем себя
-	m_feet++;
-	return copy;
+    Distance result(*this);
+    m_feet++;
+    return result;
+}
+ 
+float operator/(Distance const& lhs, Distance const& rhs)
+{
+    float inches1 = lhs.GetFeet()*12.0f + lhs.GetFeet();
+    float inches2 = rhs.GetFeet()*12.0f + rhs.GetFeet();
+    return inches1 / inches2;
 }

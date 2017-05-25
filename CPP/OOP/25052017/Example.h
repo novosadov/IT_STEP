a2fdfa4b8f2338ff2c09e0dbb69276f9c2ad675f
Pavel Novosadov <berk@mail.ru>
2017-05-25 10:09:27 +0300

    Update Example.h

--- CPP/OOP/25052017/Example.h
+++ CPP/OOP/25052017/Example.h
@@ -5,8 +5,22 @@ class A
 	int a = 0;
 public:
 	A() = default; //конструктор по умолчанию
-	A()=delete; //отключает конструктор
+//	A()=delete; //отключает конструктор
 	A(int a) :a(a) 
 	{
 	};
+};
+
+class Logger   //Создаёт логи (надо дописать код)
+{
+private:
+	static Logger* g_instance;
+	Logger() = default;
+public:
+	void Log(char const*);
+	
+	Logger(Logger const&) = delete;
+	Logger& operator=(Logger&) = delete;
+
+	static Logger* GetInstance();
 };

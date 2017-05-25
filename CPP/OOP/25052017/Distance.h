714808839ad693901c6a7d5165535cd49e0c7b30
Pavel Novosadov <berk@mail.ru>
2017-05-25 11:26:57 +0300

    Update Distance.h

--- CPP/OOP/25052017/Distance.h
+++ CPP/OOP/25052017/Distance.h
@@ -2,39 +2,32 @@
 
 class Distance
 {
-private:
-    int m_feet;
-    float m_inches;
-    
-    float ToInches() const;
-    
 public:
-    Distance();
-    Distance(int feet);
-    Distance(int feet, float inches);
-    
-    int GetFeet() const;
-    float GetInches() const;
-    void SetData(int feet, float inches);
-    void Read();
-    void Print() const;
-    void Add(Distance const& d);
-    void Deduct(Distance const& d);
-    
-    void operator+=(Distance const& rhs);
-    void operator-=(Distance const& rhs);
-    
-    Distance operator+(Distance const& rhs);
-    Distance operator-(Distance const& rhs);
-    
-    bool operator==(Distance const& rhs) const;
-    bool operator!=(Distance const& rhs) const;
-    bool operator<=(Distance const& rhs) const;
-    bool operator>(Distance const& rhs) const;
-    
-    Distance& operator++();
-    Distance operator++(int);
+	Distance();
+	Distance(int feet, float inches);
+	Distance(int feet);
+
+	int GetFeet();
+	float GetInches();
+	void SetData(int feet, float inches);
+	void Read();
+	void Print();
+	void Add(Distance const& d);
+	void Deduct(Distance const& d);
+
+
+	Distance operator-(Distance const& rhs);
+	Distance operator+(Distance const& rhs);
+	void operator+=(Distance const& rhs);
+	void operator-=(Distance const& rhs);
+	Distance& operator++();
+	Distance operator++(int);
+
+private:
+	int m_feet = 0;
+	float m_inches = 0;
 };
+
 
 //Distance Summ(Distance const& d1, Distance const& d2);
 //Distance Diff(Distance const& d1, Distance const& d2);

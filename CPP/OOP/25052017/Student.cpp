f16ffa2a23da7914dd1acdedb1e2fe0398562bad
Pavel Novosadov <berk@mail.ru>
2017-05-30 10:39:14 +0300

    Update Student.cpp

--- CPP/OOP/25052017/Student.cpp
+++ CPP/OOP/25052017/Student.cpp
@@ -3,72 +3,62 @@
 #include <cstring>
 #include <iostream>
 
-int Student::g_count=0;
+
+int Student::g_count = 0;
 
 void Student::IncreaseCounter()
 {
 	++g_count;
 }
-void Student::DereaseCounter()
+void Student::DecreaseCounter()
 {
 	--g_count;
 }
+
 int Student::GetStudentCount()
 {
 	return g_count;
 }
 
-
-Student::Student(Group& group) :
-	m_group(group),
+Student::Student() :
 	m_rating(0)
 {
 	IncreaseCounter();
-	m_name = new char[1];
-	m_name[0] = '\0';
 }
 
-Student::Student(Group& group, char const* name, int rating) :
-	m_name(NULL),
-	m_group(group),
+Student::Student(String const& name, int rating) :
+	m_name(name),
 	m_rating(rating)
 {
 	IncreaseCounter();
-	SetName(name);
 }
 
 Student::Student(Student const& student) :
-	m_group(student.m_group),
 	m_rating(student.m_rating)
 {
 	IncreaseCounter();
 	std::cout << "copy ctor\n";
-	SetName(student.GetName());
+	m_name = student.m_name;
 }
 
 Student::~Student()
 {
-	DereaseCounter();
-	delete[] m_name;
+	DecreaseCounter();
 }
 
-char const* Student::GetName() const
+String const& Student::GetName() const
 {
 	return m_name;
 }
 
 int Student::GetRating() const
 {
-
 	return m_rating;
 }
 
-void Student::SetName(char const* name)
+void Student::SetName(String const& name)
 {
-	delete[] m_name;
-	size_t length = strlen(name);
-	m_name = new char[length + 1];
-	strcpy(m_name, name);
+	m_name = name;
 }
 
 void Student::SetRating(int rating)
@@ -91,14 +81,13 @@ void Student::Print() const
 	std::cout << m_name << ": " << m_rating << "\n";
 }
 
-
 Student& Student::operator=(Student& student)
 {
 	if (this == &student)
 	{
 		return *this;
 	}
-	this->SetName(student.GetName());
+	this->m_name = student.m_name;
 	this->m_rating = student.GetRating();
 	return *this;
 }

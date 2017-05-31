c8c2604a3b5793762ef96be78f909798f16b9e07
Pavel Novosadov <berk@mail.ru>
2017-05-31 11:25:37 +0300

    Create Interval.cpp

---
+++ CPP/OOP/31052017/Interval.cpp
@@ -0,0 +1,143 @@
+#include "Interval.h"
+#include <iostream>
+
+float max(float a, float b)
+{	
+	if (a > b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+
+float min(float a, float b)
+{
+	if (a < b)
+	{
+		return a;
+	}
+	else
+	{
+		return b;
+	}
+}
+
+
+Interval::Interval()
+{
+	m_begin = 0;
+	m_end = 0;
+}
+
+Interval::Interval(float begin, float end)
+{
+	m_begin = begin;
+	m_end = end;
+}
+
+
+Interval::Interval(Interval const& interval):
+	m_begin(interval.m_begin), 
+	m_end(interval.m_end)
+{			
+}
+
+Interval& Interval::operator=(Interval const& interval)
+{
+	if (this == &interval)
+	{
+		return *this;
+	}
+	this->m_begin = interval.m_begin;
+	this->m_end = interval.m_end;
+	return *this;
+}
+
+float Interval::SetBeginEnd(float begin, float end)
+{
+	m_begin = begin;
+	m_end = end;
+} 
+
+float Interval::GetBegin()const
+{
+	return m_begin;
+}
+
+float Interval::GetEnd()const
+{
+	return m_end;
+}
+
+
+float Interval::GetLength() const
+{
+	
+	return m_end - m_begin;
+}
+
+void Interval::Move(float dist)
+{
+
+}
+
+void Interval::Expand(float scale)
+{
+
+}
+
+float Interval::Mid() const
+{
+
+}
+
+bool Interval::IsEmpty() const
+{
+
+}
+
+
+Interval Interval::operator&(Interval const& rhs)
+{
+	Interval result;
+	float beg3 = max(this->m_begin, rhs.m_begin);
+	float end3 = min(this->m_end, rhs.m_end);
+
+	if (beg3 <= end3)
+	{
+		result.m_begin = beg3;
+		result.m_end = end3;
+	}
+	return result;
+}
+
+Interval Interval::operator|(Interval const& rhs)
+{
+	Interval result;
+
+}
+
+void Interval::operator+=(Interval const rhs)
+{
+
+}
+
+void Interval::operator-=(Interval const rhs)
+{
+
+}
+
+std::ostream& operator<<(std::ostream& stream, Interval const& interval)
+{
+	stream << interval.GetBegin << " '" << interval.GetEnd<< "\n";
+	return stream;
+}
+
+std::istream& operator >> (std::istream& stream, Interval& interval)
+{
+
+}

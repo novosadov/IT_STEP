dce4df0b270caa9891f381d10fc7fb3ac5fcf927
Pavel Novosadov <berk@mail.ru>
2017-06-02 15:53:00 +0300

    Update Vector.h

--- CPP/HomeTask/29052017/Vector.h
+++ CPP/HomeTask/29052017/Vector.h
@@ -12,4 +12,9 @@ public:
 	void Read();
 	void Print() const;
 	Vector operator+(Vector const& rhs);
+	int Summ(int v1, int v2);
+	int LeghtVector(int x, int y, int z);
+	Vector Scalyar(Vector v1, int x1, int y1, int z1, Vector v2, int x2, int y2, int z2);
+	Vector operator*(Vector const& rhs);
+	Vector& operator=(Vector const& vector);
 };

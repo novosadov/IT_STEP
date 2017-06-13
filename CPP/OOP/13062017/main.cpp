1d86690682a8192b2336b24c2ade400abae51e27
Pavel Novosadov <berk@mail.ru>
2017-06-13 11:57:43 +0300

    Update main.cpp

--- CPP/OOP/13062017/main.cpp
+++ CPP/OOP/13062017/main.cpp
@@ -1,18 +1,24 @@
-#include <iostream>
 #include "DynArray.h"
-
+ 
+#include <iostream>
+ 
 int main()
 {
-	DynArray<int> d = { 200, 300, 100 };
-
-	DynArray<int> d1 = d * 5;
-	std::cout << d1.GetCount() << "\n";
-
-	/*int arr[] = { 5, 6, 7, 8 };
-	for (int el : arr)
-	{
-		std::cout << el << " ";
-	}*/
-
-	return 0;
+    DynArray<int> d = {200, 300, 100};
+ 
+    DynArray<int> d1 = d * 5;
+    std::cout << d1.GetCount() << "\n";
+ 
+    /*int arr[] = { 5, 6, 7, 8 };
+    for (int el : arr)
+    {
+        std::cout << el << " ";
+    } */
+ 
+    DynArray<int> const d2(d1);
+    std::cout << d2[0] << "\n";
+    d1[0] = -5;
+    std::cout << d1[0] << "\n";
+ 
+    return 0;
 }

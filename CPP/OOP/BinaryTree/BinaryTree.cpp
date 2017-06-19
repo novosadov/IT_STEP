ef556eeab20eaaecd8bc83a065557193e4d32c5f
Pavel Novosadov <berk@mail.ru>
2017-06-19 11:48:42 +0300

    Update BinaryTree.cpp

--- CPP/OOP/BinaryTree/BinaryTree.cpp
+++ CPP/OOP/BinaryTree/BinaryTree.cpp
@@ -20,5 +20,27 @@ int main()
 		std::cout << "Found \n";
 	}
 
+	return 0;#include<iostream>
+#include"BinaryTree.h"
+
+
+int main()
+{
+	BinaryTree Tree;
+	Tree.Insert(8);
+	Tree.Insert(5);
+	Tree.Insert(3);
+	Tree.Insert(10);
+	Tree.Insert(12);
+	Tree.Insert(0);
+
+	Tree.Print();
+
+	TreeNode* node = Tree.Find(3);
+	Tree.Erase(node);
+	std::cout << "\n";
+	Tree.Print();
+
 	return 0;
+}
 }

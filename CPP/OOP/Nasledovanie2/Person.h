3ba30a28566d0832ec85b40420f551167017099a
Pavel Novosadov <berk@mail.ru>
2017-06-22 10:01:32 +0300

    Create Person.h

---
+++ CPP/OOP/Nasledovanie2/Person.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "String.h"
+
+class Person
+{
+protected:
+	String m_name;
+public:
+	Person();
+	~Person();
+
+	String const& GetName() const;
+	Person(String const& name);
+	void SetName(String const& name);
+};
+
+

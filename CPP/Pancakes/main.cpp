20d69043d60184d1f81b1ba0ecfc42a2cbe6064b
Pavel Novosadov <berk@mail.ru>
2017-06-27 11:44:03 +0300

    Update main.cpp

--- CPP/Pancakes/main.cpp
+++ CPP/Pancakes/main.cpp
@@ -1,47 +1,54 @@
-#include<iostream>
-#include"Order.h"
-
-
+#include "Order.h"
+#include <iostream>
+ 
 enum SupplimentType
 {
-	Bacon,
-	Onion,
-	Chocolate,
-	Jam
+    Bacon,
+    Onion,
+    Chocolate,
+    Jam
 };
-
+ 
 IOrder* AddSuppliment(IOrder* order, SupplimentType type, size_t count)
 {
-	switch (type)
-	{
-	case Bacon:		
-		return new BaconSup(order, count);		
-	case Onion:
-		return new OnionSup(order, count);
-	case Chocolate:
-		return new ChocolateSup(order, count);
-	case Jam:
-		return new JamSup(order, count);
-	default:
-		return order;
-	}
+    switch (type)
+    {
+    case Bacon:
+        return new BaconSup(order, count);
+    case Onion:
+        return new OnionSup(order, count);
+    case Chocolate:
+        return new ChocolateSup(order, count);
+    case Jam:
+        return new JamSup(order, count);
+    default:
+        return order;
+    }
 }
-
+ 
 void PrintCheck(IOrder* order)
 {
-	std::cout << "Your order:\n" <<
-		order->GetName() << "\n\n" <<
-		"Cost: " << order->GetCost() << "\n";
+    std::cout << "Your order:\n" <<
+        order->GetName() << "\n\n" <<
+        "Cost: " << order->GetCost() << "\n";
 }
-
+ 
+IOrder* ApplyDiscount(IOrder* order)
+{
+    return new Discount(order);
+}
+ 
 int main()
 {
-	IOrder* order = new CheeseAndTomatoPancake();
-	order = AddSuppliment(order, Bacon, 2);	
-	order = AddSuppliment(order, Onion, 1);
-
-	PrintCheck(order);
-	delete order;
-
-	return 0;
+    IOrder* order = new CheeseAndTomatoPancake();
+    order = AddSuppliment(order, Bacon, 3);
+    order = AddSuppliment(order, Onion, 2);
+ 
+    //order = ApplyDiscount(order);
+ 
+    PrintCheck(order);
+    delete order;
+ 
+ 
+    return 0;
 }

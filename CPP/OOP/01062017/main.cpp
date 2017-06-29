57419f447ea890105f486e83d3998842a5b95b26
Pavel Novosadov <berk@mail.ru>
2017-06-29 09:58:20 +0300

    Update main.cpp

--- CPP/OOP/01062017/main.cpp
+++ CPP/OOP/01062017/main.cpp
@@ -5,6 +5,30 @@
 
 int main()
 {
-	
+	Stack stack;
+	try
+	{
+		stack.Push(1);
+		stack.Push(2);
+		int a = stack.Pop();
+		a = stack.Pop();
+		a = stack.Pop();
+		std::cout << "1\n";
+	}
+	catch (char* message)
+	{
+		std::cout << message<<std::endl;
+	}
+
+	catch (std::size_t count)
+	{
+		std::cout << count << std::endl;
+	}
+
+	catch (StackException& ex)
+	{
+		std::cout << "Stack throwed exception\n" <<
+			"count = " << ex.GetCount() << "\n";
+	}
 	return 0;
 }

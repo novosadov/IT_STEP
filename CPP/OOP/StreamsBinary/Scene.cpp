d91e866442794d1ea973aeb9631a5b4dfecfc1c3
Pavel Novosadov <berk@mail.ru>
2017-07-06 11:16:31 +0300

    Update Scene.cpp

--- CPP/OOP/StreamsBinary/Scene.cpp
+++ CPP/OOP/StreamsBinary/Scene.cpp
@@ -1,30 +1,69 @@
 #include "Scene.h"
 #include "Shapes.h"
 #include <iostream>
-
+ 
 Scene::~Scene()
 {
-	Clear();
+    Clear();
 }
-
+ 
 void Scene::Add(IShape* shape)
 {
-	m_shapes.push_back(shape);
+    m_shapes.push_back(shape);
 }
-
+ 
 void Scene::Clear()
 {
-	for (IShape* shape : m_shapes)
-	{
-		delete shape;
-	}
-	m_shapes.clear();
+    for (IShape* shape : m_shapes)
+    {
+        delete shape;
+    }
+    m_shapes.clear();
 }
-
+ 
 void Scene::Print(IGraphics* graphics)
 {
-	for (IShape* shape : m_shapes)
-	{
-		shape->Draw(graphics);
-	}
+    for (IShape* shape : m_shapes)
+    {
+        shape->Draw(graphics);
+    }
+}
+ 
+void Scene::SaveToStream(std::ostream& stream)
+{
+    int id = StreamIdSceneBegin;
+    stream.write(reinterpret_cast<char*>(&id), sizeof(id));
+    for (IShape* shape : m_shapes)
+    {
+        shape->SaveToStream(stream);
+    }
+    id = StreamIdSceneEnd;
+    stream.write(reinterpret_cast<char*>(&id), sizeof(id));
+}
+ 
+void Scene::LoadFromStream(std::istream& stream)
+{
+    int id;
+    int size;
+ 
+    stream.read(reinterpret_cast<char*>(&id), sizeof(id));
+    if (id != StreamIdSceneBegin)
+    {
+        throw ShapeStreamReadException("Scene");
+    }
+ 
+    while (true)
+    {
+        stream.read(reinterpret_cast<char*>(&id), sizeof(id));
+        stream.read(reinterpret_cast<char*>(&size), sizeof(size));
+        IShape* shape = nullptr;
+ 
+        switch (id)
+        {
+        case StreamIdCircle:
+            shape = new Circle();
+        default:
+            break;
+        }
+    }
 }

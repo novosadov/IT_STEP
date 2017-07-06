e6c1e8c62dc9d434b4b94e826b69cb26dc66f4b2
Pavel Novosadov <berk@mail.ru>
2017-07-06 10:34:52 +0300

    Create Scene.h

---
+++ CPP/OOP/StreamsBinary/Scene.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "IShape.h"
+#include <list>
+#include <iosfwd>
+
+class Scene
+{
+	std::list<IShape*> m_shapes;
+
+public:
+	~Scene();
+
+	void Add(IShape* shape);
+	void Clear();
+	void Print(IGraphics* graphics);
+};
